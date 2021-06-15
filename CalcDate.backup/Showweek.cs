0f6c26d41027a8a499203ac0c30c845202bc63b3
yzqde <yzqdev@outlook.com>
2021-06-15 17:29:28 +0800

    更新到.net5

---
+++ CalcDate.backup/Showweek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CalcDate
+{
+    internal class Showweek
+    {
+        public static void showweek()
+        {
+            Console.Clear();
+            while (true)
+            {
+                startx:
+                string s;
+                StreamReader sr = new StreamReader(@"week.txt");
+                Console.WriteLine("星期几：");
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Regex reg = new Regex(@"\d\d\d\d-\d\d-\d\d");
+                    Match m = reg.Match(s);
+                    DateTime dateTime = DateTime.Parse(m.Value);
+                    Day week = (Day)((int)dateTime.DayOfWeek);
+                    Console.WriteLine(dateTime.ToString("yyyy-MM-dd") + "是" + "{0,10}", week);
+                }
+                Console.WriteLine("输入0退出");
+                string st;
+                while ((st = Console.ReadLine()) != null)
+                {
+                    if (st.Equals("0")) break;
+                    else Console.WriteLine("错误,请再试一次");
+                }
+                break;
+            }
+            Console.Clear();
+        }
+
+        public enum Day { Sun = 0, Mon, Tue, Wed, Thu, Fri, Sat }   //枚举类型
+    }
+}
---
+++ doyoulikeme.backup/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            button3.Visible = true;
+            button2.Visible = false;
+        }
+
+        private void button3_MouseMove(object sender, MouseEventArgs e)
+        {
+            button3.Visible = false;
+            button2.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("这么巧，我也喜欢你！");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("你还是喜欢我~");
+        }
+    }
+}
